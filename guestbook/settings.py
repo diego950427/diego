f441822ff270993f514f07b5098ac1d6576eb9d9
diego <diegolin20427@gmail.com>
2022-12-23 10:50:13 +0800

    Create ok

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qr@y7+a9jrkl_sghws3&qm=)rst_xkc@hs*yfrsyg4swo3+r)&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'veb',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant'
 
 TIME_ZONE = 'UTC'
 
